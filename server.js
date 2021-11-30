9eceeab3b214a271882601d2baade7a7326fa52b
yuliaelkina <52623041+yuliaelkina@users.noreply.github.com>
2021-11-30 10:46:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ class TicketsController {
   }
 
   findTicket(id) {
-      const answer = parseInt(id);
+      const answer = this.fullTickets.find((el) => el.id === parseInt(id));
       if (answer) {
         return answer;
       } else {
