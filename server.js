dfd60ef9cbc491c4f66edfa177220ad32fbe5cb2
yuliaelkina <52623041+yuliaelkina@users.noreply.github.com>
2022-01-28 12:03:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const Koa = require('koa');
+const Router = require('koa-router');
 const koaBody = require('koa-body');
 const app = new Koa();
 const cors = require('koa2-cors');
+const router = new Router();
 const port = process.env.PORT || 7070;
 
 app.use(koaBody({
@@ -21,104 +23,59 @@ app.use(
   })
 );
 
-class Ticket {
-    constructor(name, status, id, created) {
-      this.name = name;
-      this.status = status;
-      this.created = created;
-      this.id = id;
-    }
-};
-
-class FullTicket {
-    constructor(name, status, description, id) {
-      this.name = name;
-      this.status = status;
-      this.description = description;
-      this.id = id;
-      this.created = new Date().getTime();
-    }
-
-    createTicket() {
-      return new Ticket(this.name, this.status, this.id, this.created);
-    }
-
-};
-
-class TicketsController {
+class Chat {
   constructor() {
-    this.index = 1000;
-    this.tickets = [];
-    this.fullTickets = [];
+    this.users = [];
+    this.messages = [];
   }
-
-  createTickets(name, status, description) {
-    if (name && status && description) {
-        const ticket = new FullTicket(name, status, description, this.index);
-        this.index += 1;
-        this.fullTickets.push(ticket);
-        this.tickets.push(ticket.createTicket());
-        return "ticket created";
-    } else {
-        return "uncorrect data";
+  addUser(nickname) {
+    if (this.users.includes(nickname)) {
+      return 'nickname has already exist';
     }
+    this.users.push(nickname);
+    return 'ok';
+  }
+  deleteUser(Nickname) {
+    const index = this.users.indexOf(el => {
+      el == Nickname;
+    });
+    this.users.splice(index, 1);
   }
 
-  deleteTickets(id) {
-    const fullTicket = this.fullTickets.find((el) => el.id === parseInt(id));
-    const ticket = this.tickets.find((el) => el.id === parseInt(id));
-       if (ticket && fullTicket) {
-      this.tickets.splice(ticket, 1);
-      this.fullTickets.splice(fullTicket, 1);
-      return "deleted";
-    } else {
-      return "Ticket with this Id doesn't exist";
-    }
-  }
-
-  findTicket(id) {
-      const answer = this.fullTickets.find((el) => el.id === parseInt(id));
-      if (answer != undefined) {
-        return answer;
-      } else {
-          return "ticket with this Id doesn't exist";
-      }
+  addMessage(nickname, messageText) {
+    const message = new Message(nickname, messageText);
+    this.messages.push(message);
   }
   
-  changeStatus(id) {
-      const fullTicket = this.fullTickets.find((el) => el.id === parseInt(id));
-      const ticket = this.tickets.find((el) => el.id === parseInt(id));
-      if (ticket && fullTicket) {
-        if (ticket.status === 'true') {
-          ticket.status = 'false';
-          fullTicket.status = 'false';
-        } else {
-          ticket.status = 'true';
-          fullTicket.status = 'true';
-        }
-        return ticket.status;
-      } else {
-          return "something went wrong"
-      }
+  returnMessageList() {
+    const list = [];
+    this.messages.forEach((el) => {
+      list.push(el.returnMessage());
+    });
+    return list;
   }
-    
-  updateTicket(id, name, description) {
-    const fullTicket = this.fullTickets.find((el) => el.id === parseInt(id));
-    const ticket = this.tickets.find((el) => el.id === parseInt(id));
-    if (ticket && fullTicket) {
-      ticket.name = name;
-      fullTicket.name = name;
-      fullTicket.description = description;
-      return ticket.name;
-    } else {
-      return "ticket with this Id doesn't exist";
-    }
+  
+};
+class Message {
+  constructor (nickname, messageText) {
+    this.nickName = nickname;
+    this.text = messageText;
+    this.date = this.addDate();
   }
 
+  addDate() {
+    const now = new Date();
+    return `${now.getHours}:${now.getMinutes} ${now.getDate}.${now.getMonth}.${now.getFullYear}`
+  }
+
+  returnMessage() {
+    return {nickname: `${this.nickName}`, time: `${this.text}`, date: `${this.date}`}
+  }
 }
-const ticketsController = new TicketsController();
-ticketsController.createTickets('Task1', 'false', 'Task 1 description should be here');
-ticketsController.createTickets('Task2', 'false', 'Task 2 description should be here');
+
+const chat = new Chat();
+const testMessage = new Message('admin', ' Добро пожаловать в чат');
+
 
 
 app.use(async ctx => {
